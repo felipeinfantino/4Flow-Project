957c3b4e35ab7e71f641453f3328258da62db407
SEBASTIAN BRITO <s.britocorral@gmail.com>
2019-11-05 00:25:03 +0100

    Add authorization logic

--- 4FlowDashboards/src/App.js
+++ 4FlowDashboards/src/App.js
@@ -2,51 +2,60 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {
-  BrowserRouter,
-  Route,
-  Link,
-  Switch,
+    BrowserRouter,
+    Route,
+    Link,
+    Switch,
 } from 'react-router-dom'
 import Planer from './components/Planer';
 import About from './components/About';
 import Balancer from './components/Balancer';
 
-document.body.style = 'background: black;'; 
+import Login from "./components/auth/Login";
+import SignUp from "./components/auth/SignUp";
+import {AuthProvider} from "./Auth";
+import PrivateRoute from "./PrivateRoute";
+
+document.body.style = 'background: black;';
 
 export const columnNames = {
-  TO_DO : 'To do',
-  IN_PROGRESS : 'In progress',
-  WAITING : 'Waiting',
-  DONE: 'Done',
-}
+    TO_DO: 'To do',
+    IN_PROGRESS: 'In progress',
+    WAITING: 'Waiting',
+    DONE: 'Done',
+};
 
 class App extends React.Component {
 
-render() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="App-title">Admin</h1>
-      </header>
-      <BrowserRouter>
-      <div className="menu">
-          <ul>
-            <li> <Link to="/">Planer</Link> </li>
-            <li> <Link to="/balancer">Balancer</Link> </li>
-            <li> <Link to="/about">About</Link> </li>
-          </ul>
-      </div>
-      <div className="App-intro">
-          <Switch>
-            <Route exact path="/" component={Planer} />
-            <Route path="/balancer" component={Balancer} />
-            <Route path="/about" component={About} />
-          </Switch>
-        </div>
-        </BrowserRouter>
-    </div>
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1 className="App-title">Admin</h1>
+                </header>
+                <AuthProvider>
+                    <BrowserRouter>
+                        <div className="menu">
+                            <ul>
+                                <li><Link to="/">Planer</Link></li>
+                                <li><Link to="/balancer">Balancer</Link></li>
+                                <li><Link to="/about">About</Link></li>
+                            </ul>
+                        </div>
+                        <div className="App-intro">
+                            <Switch>
+                                <PrivateRoute exact path="/" component={Planer}/>
+                                <PrivateRoute path="/balancer" component={Balancer}/>
+                                <PrivateRoute path="/about" component={About}/>
+                                <PrivateRoute exact path="/signup" component={SignUp}/>
+                                <Route exact path="/login" component={Login}/>
+                            </Switch>
+                        </div>
+                    </BrowserRouter>
+                </AuthProvider>
+            </div>
+        );
+    }
+}
 
-  );
-}
-}
 export default App;
