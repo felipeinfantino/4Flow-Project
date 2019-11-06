59e1f99b2495f2cf98d4e4983ace51146a605ee7
SEBASTIAN BRITO <s.britocorral@gmail.com>
2019-11-06 11:33:56 +0100

    Save firebase settings hardcoded

--- 4FlowDashboards/src/Firebase.js
+++ 4FlowDashboards/src/Firebase.js
@@ -4,14 +4,14 @@ require('firebase/auth');
 require('firebase/database');
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_DATABASE_URL,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
-    measurementId: process.env.REACT_APP_MEASUREMENT_ID
+    apiKey: "AIzaSyBcdL_1pVMBFcJgaiiuQCIM8YfjbUADjeg",
+    authDomain: "flowdashboard-6c548.firebaseapp.com",
+    databaseURL: "https://flowdashboard-6c548.firebaseio.com",
+    projectId: "flowdashboard-6c548",
+    storageBucket: "flowdashboard-6c548.appspot.com",
+    messagingSenderId: "585682988528",
+    appId: "1:585682988528:web:3ef7bf657ce42f305ec135",
+    measurementId: "G-X4WY4T8R28"
 };
 
 const firebase = firebaseApp.initializeApp(firebaseConfig);
