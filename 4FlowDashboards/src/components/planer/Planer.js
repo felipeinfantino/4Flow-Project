3e38507a5baadf85393b1b183bbd3ecbe4694c86
felipeinfantino <felipe.infantino@hotmail.com>
2019-11-13 09:21:38 +0100

    compile error fixed

--- 4FlowDashboards/src/components/planer/Planer.js
+++ 4FlowDashboards/src/components/planer/Planer.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './Planer.css';
 import Todos from '../dashboard/Todos';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AddTodo from '../dashboard/AddTodo';
