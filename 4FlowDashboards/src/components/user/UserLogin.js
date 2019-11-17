c196596eaacc557f25d091ff0d549004a737a288
nanhduc <ad.nguyen@bohmeyer-schuster.com>
2019-11-17 18:49:10 +0100

    WIP show feedback after reset

--- 4FlowDashboards/src/components/user/UserLogin.js
+++ 4FlowDashboards/src/components/user/UserLogin.js
@@ -6,7 +6,6 @@ import {Form, Row, Col, Button, Container} from 'react-bootstrap';
 import './Login.css';
 
 const UserLoginForm = () => {
-
     const {currentUser} = useContext(AuthContext);
 
     const handleLogin = (event) => {
@@ -22,6 +21,16 @@ const UserLoginForm = () => {
         }
     };
 
+    function resetPassword() {
+        let mail = document.getElementById('mail').value ;
+        
+        return firebase.auth().sendPasswordResetEmail(mail).then(function() {
+            // Email sent.
+          }).catch(function(error) {
+            // An error happened.
+          });          
+    }
+
     if (currentUser) {
         return <Redirect to="/"/>;
     }
@@ -36,7 +45,6 @@ const UserLoginForm = () => {
                             <Form.Group controlId="formBasicEmail">
                                 <Form.Control type="email" placeholder="Enter email" className="fadeIn second"/>
                             </Form.Group>
-
                             <Form.Group controlId="formBasicPassword">
                                 <Form.Control type="password" placeholder="Password" className="fadeIn third"/>
                             </Form.Group>
@@ -49,7 +57,17 @@ const UserLoginForm = () => {
                         </Form>
                     </div>
                     <div className="formFooter">
-                        <a className="forget-password" href="/reset-password">Forgot Password?</a>
+                        <p style={{ fontSize: '14px' }}>Forgot Password?</p>
+                        <div className="row" style={{ marginTop: '10px' }}>
+                            <div className="col-md-9">
+                                <input className="form-control" id="mail" type="email" placeholder="Enter email"></input>
+                            </div>
+                            <div className="col-md-3">
+                                <button style={{ marginLeft: '5px' }} className="btn btn-sm btn-secondary" onClick={resetPassword}>
+                                    Reset
+                                </button>
+                            </div>
+                        </div>
                     </div>
                 </Col>
             </Row>
