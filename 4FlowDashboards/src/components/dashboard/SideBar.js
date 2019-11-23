ef216e3319cf8770c01fb6e31df797db0ce827b2
arehmansaeed <arehmansaeed@babbel.com>
2019-11-23 22:22:35 +0100

    Removed unwanted code from sidebar.js

--- 4FlowDashboards/src/components/dashboard/SideBar.js
+++ 4FlowDashboards/src/components/dashboard/SideBar.js
@@ -1,67 +1,21 @@
-import React, {Component, useContext} from 'react'
+import React, {Component} from 'react'
 import {slide as Menu} from 'react-burger-menu';
-import firebase from "../firebase/Firebase";
-import {Redirect} from "react-router";
-import {AuthContext} from "../auth/Auth";
 import './SideBar.css';
-export const SideBar = () => {
-const auth = useContext(AuthContext);
 
-
-let user = firebase.auth().currentUser;
-let email, userRoles, roleRedirect;
-
-if (user != null) {
-  console.log(user);
-  email = user.email;
-  console.log(email);
-}
-
-async function myPromise() {
-  return new Promise((resolve, reject) => {
-    var usersRef = firebase.database().ref("users/");
-    usersRef.on("child_added", function(data) {
-      var newVal = data.val();
-      if(newVal.email === email) {
-        userRoles = newVal.userRole;
-        resolve(userRoles);
-      }
-    })
-  })   
-}
-
-async function callingDB() {
-  try {
-    let callingDBFunction = await myPromise(); 
-    console.log(callingDBFunction); 
-    userRoles = callingDBFunction;
-    console.log(userRoles);
-    if(userRoles==="planer") {
-      roleRedirect = "/planner";
-    }
-    else if(userRoles === "balancer") {
-      roleRedirect = "/balancer";
-    }
-    else {
-      roleRedirect = "/admin";
-    }
-  }
-  catch(error) {
-    console.log(error);
-  }
-}
-
-callingDB();
-    
+export class SideBar extends Component {
+    render() {
         return (
-            
-           <Menu>
-              <a className="menu-item" href="/link2">
+            <Menu>
+                <a className="menu-item" href="/link1">
+                    Placeholder
+                </a>
+
+                <a className="menu-item" href="/link2">
                     Placeholder 2
                 </a>
             </Menu>
-      
         );
-};
+    }
+}
 
-export default SideBar;
+export default SideBar
