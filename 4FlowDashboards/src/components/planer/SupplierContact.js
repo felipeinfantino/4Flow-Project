28939874a0426825bd68ef8111f149433144414b
Rene Enjilian <enjilianrene@gmail.com>
2019-12-08 16:06:33 +0100

    dynamic request

--- 4FlowDashboards/src/components/planer/SupplierContact.js
+++ 4FlowDashboards/src/components/planer/SupplierContact.js
@@ -1,55 +1,75 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import firebase from "../firebase/Firebase";
 import axios from "axios";
 // const cryptoRandomString = require('crypto-random-string');
-var db =  firebase.firestore();
+var db = firebase.firestore();
 
 export class SupplierContact extends Component {
     state = {
         startDate: new Date()
-      };
-    
-    
-      handleChange = date => {
+    };
+
+
+    handleChange = date => {
         this.setState({
-          startDate: date
+            startDate: date
         });
-      };
-      handleSubmit = (event) => {
+    };
+    handleSubmit = async (event) => {
         event.preventDefault();
         //let suppliersCheckBox = document.getElementsByName('suppliers[]');
         let date = document.getElementsByName('datepicker')[0].value;
         let type = document.getElementsByName('contract_type')[0].value;
         let subject = document.getElementsByName('subject')[0].value;
-        let body = document.getElementsByName('mailBody')[0].value;
+        let mailBody = document.getElementsByName('mailBody')[0].value;
         let tid = 'taskID';
 
-        let suppliers = ['google', 'amazon']
+        let suppliersList = ['DHL']
+
+        let tester = await db.collection('Users').doc('User-Data').get()
+
         
-        //console.log(suppliers)
+        let data = tester.data()
+        let keys = Object.keys(data)
+        let suppliers = []
+        keys.forEach((key) => {
+            let tempObject = data[key]
+            console.log(tempObject)
+            if ('companyName' in tempObject) {
+                
+                if (suppliersList.indexOf(tempObject.companyName) != -1) {
+                    console.log("HELLLOOOOO")
+                    console.log(tempObject.email)
+                     suppliers.push(tempObject.email)
+                }
+            }
+        })
+
+        
+
+
         /*suppliersCheckBox.forEach((element) => {
             if (element.checked) {
                 suppliers.push(element.value);
             }
         });*/
-        
+        //console.log("HELLOOOOOO")
         suppliers.forEach(supplier => {
             // var newKey = cryptoRandomString({length: 20});
 
             var data = {
                 customerID:
-                    {
-                        mail: supplier,
-                        date: date,
-                        type: type,
-                        subject: subject,
-                        body: body
-                    }
+                {
+                    mail: supplier,
+                    date: date,
+                    type: type,
+                    subject: subject,
+                    body: mailBody
+                }
             };
 
-            console.log(data)
 
             //Aufträge=>TaskID=>CustomerID=>Data
             db.collection("Aufträge").doc(tid).set(data).then(function (response) {
@@ -67,14 +87,14 @@ export class SupplierContact extends Component {
                 body: JSON.stringify({
                     destinations: ["test@gmail.com"],
                     subject: subject,
-                    text: body
+                    text: mailBody
                 })
             }).then(function (response) {
                 console.log(response)
             });
         });
-      }
-    
+    }
+
     render() {
         return (
             <div id="contact-template">
@@ -82,8 +102,8 @@ export class SupplierContact extends Component {
                     <form onSubmit={this.handleSubmit}>
                         <div className="row">
                             <div className="col-md-3">
-                                
-                            </div> 
+
+                            </div>
                             <div className="col-md-3">
                                 <div className="form-group" style={{ textAlign: 'left', marginTop: '40px' }}>
                                     <div className="row">
@@ -118,7 +138,7 @@ export class SupplierContact extends Component {
                                 </div>
                             </div>
                             <div className="col-md-6">
-                                <div className="form-group" style={{ marginTop: '40px', marginLeft: '100px', width: '70%'}}>
+                                <div className="form-group" style={{ marginTop: '40px', marginLeft: '100px', width: '70%' }}>
                                     <div className="row">
                                         <label className="control-label" style={{ textAlign: 'left' }}><strong>Mail subject:</strong></label>
                                     </div>
