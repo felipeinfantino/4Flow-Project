7719fadbf1d5f920e1a06467ed0019b54725bb61
samirka9767 <samirehuseyinli@gmail.com>
2020-01-07 23:00:26 +0100

    Form to add new customers/companies

---
+++ 4FlowDashboards/src/components/planer/Form.js
@@ -0,0 +1,57 @@
+import React, {useState} from 'react'
+import firebase from "../firebase/Firebase";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+var db =  firebase.firestore();
+
+function Form(props){
+    
+    const [companyData, setCompanyData] = useState({
+        name: "",
+        email: "",
+        phone: "",
+        address: "",
+        type: ""
+    });
+    
+    function handleChange(event){
+        const { name, value } = event.target;
+        console.log(event.target);
+
+        setCompanyData(prevValue => {
+          return {
+            ...prevValue,
+            [name]: value
+          };
+        });
+    }
+    return <div className="addingCompany">
+                <form className="add-comapny-form">
+                    <input name="name" placeholder="Enter company name" onChange={handleChange} value={companyData.name}></input>
+                    <input name="email" placeholder="Enter Email address" onChange={handleChange} value={companyData.email}></input>
+                    <input name="phone" placeholder="Enter phone number" onChange={handleChange} value={companyData.phone}></input>
+                    <input name="address" placeholder="Enter address" onChange={handleChange} value={companyData.address}></input>
+                    <select name="type" onChange={handleChange} value={companyData.type}>
+                        <option value="1">Supplier</option>
+                        <option value="2">Carrier</option>
+                        <option value="3">Hub Provider</option>
+                    </select>
+                    <button className="btn btn-light" style={{height: "28px",marginRight: "2px",fontSize: "13px"}} type="submit" onClick={event =>{
+                            event.preventDefault();
+                            props.onHandleClick(companyData);
+                            setCompanyData({
+                                name: "",
+                                email: "",
+                                phone: "",
+                                address: "",
+                                type: ""
+                            })
+                    }}>Add</button>
+                </form>
+        </div>
+}
+
+
+
+export default Form;
