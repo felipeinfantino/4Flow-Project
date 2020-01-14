938cd76cd73f2973778d69be2413237e723747a3
Rene Enjilian <enjilianrene@gmail.com>
2020-01-14 15:43:35 +0100

    UI is done

--- 4FlowDashboards/src/components/external/ErrorFormular.js
+++ 4FlowDashboards/src/components/external/ErrorFormular.js
@@ -1,33 +1,37 @@
 import React, {Component} from 'react'
-import {Form} from 'react-bootstrap'
+import {Form, Button} from 'react-bootstrap'
+import './ErrorFormular.css'
 class ErrorFormular extends Component{
     render(){
         return(
-    <Form>
+        <Form className="form-problem">
+            <Form.Group controlId="exampleForm.ControlSelect1" className="problem-select">
+                <Form.Label>Select type of problem</Form.Label>
+                <Form.Control as="select">
+                <option>Production Problems</option>
+                <option>Delay in delivery</option>
+                <option>insufficient time</option>
+                </Form.Control>
+            </Form.Group>
+            <Form.Group controlId="exampleForm.ControlSelect2" className="planer-select">
+                <Form.Label>Choose planer</Form.Label>
+                <Form.Control as="select" >
+                <option>Planer1</option>
+                <option>Planer2</option>
+                <option>Planer3</option>
+                <option>Planer4</option>
+                <option>Planer5</option>
+                </Form.Control>
+            </Form.Group>
+            <Form.Group controlId="exampleForm.ControlTextarea1" className="textfield-problem">
+                <Form.Label>Describe your problem</Form.Label>
+                <Form.Control as="textarea" placeholder="Your text here..." rows="10" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+                Send
+            </Button>
+        </Form>
 
-  <Form.Group controlId="exampleForm.ControlSelect1">
-    <Form.Label>Categories</Form.Label>
-    <Form.Control as="select">
-      <option>Production prroblems</option>
-      <option>Change date</option>
-      <option>Transport takes more time</option>
-    </Form.Control>
-  </Form.Group>
-  <Form.Group controlId="exampleForm.ControlTextarea1">
-    <Form.Label>Problem discription</Form.Label>
-    <Form.Control as="textarea" rows="3" />
-  </Form.Group>
-  <Form.Group controlId="exampleForm.ControlSelect2">
-    <Form.Label>Select planer</Form.Label>
-    <Form.Control as="select" >
-      <option>Planer1</option>
-      <option>Planer2</option>
-      <option>Planer3</option>
-      <option>Planer4</option>
-      <option>Planer5</option>
-    </Form.Control>
-  </Form.Group>
-</Form>
         )
     }
 }
