020368516a0a0732c658d31ac6266c7038e0dadc
felipeinfantino <felipe.infantino@hotmail.com>
2020-01-15 16:54:01 +0100

    email send to admin implemented

--- 4FlowDashboards/src/components/external/ErrorFormular.js
+++ 4FlowDashboards/src/components/external/ErrorFormular.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import {Form, Button} from 'react-bootstrap'
 import './ErrorFormular.css'
+import firebase from "../firebase/Firebase";
 
 
 const initialState = {
@@ -14,6 +15,36 @@ const initialState = {
 class ErrorFormular extends Component{
     state = {...initialState};
 
+    sendEmail = async() =>{
+        if(this.state.typeOfProblem == "" || this.state.planer == ""  || this.state.mailBody == ""){
+            alert("Please fill all fields");
+            return 
+        }
+        const mailBody = "Type of problem : " + this.state.typeOfProblem + "\n"
+                        +"Planer involved : " + this.state.planer + "\n"
+                        +"Mail body : " + this.state.mailBody + "\n";  
+        // notify boss
+        try{
+            const response = await fetch('http://localhost:3001/email', {
+                method: 'POST',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    destinations: ["amostuproject@gmail.com"],
+                    subject: "Error report",
+                    text: mailBody
+                })
+    });
+    console.log(response)
+    alert("email successfully sent");
+    this.setState(initialState);
+        }catch(e){
+            console.log("error ", e);
+        }
+       
+    }
 
     selectProblem = (e, object) => {
         //this.state.typeOfProblem = e.target.value
@@ -29,20 +60,6 @@ class ErrorFormular extends Component{
     updateTextBody = (e) => {
         console.log(e.target.value)
         this.setState({mailBody: e.target.value})
-    }
-
-
-    printState = () => {
-        if(this.state.typeOfProblem == "" || this.state.planer == ""  || this.state.mailBody == ""){
-            return 
-        }else{
-            console.log(this.state);
-            //this.setState({typeOfProblem: "", planer: "", mailBody: ""})
-            this.setState({...initialState});
-            //this.setState({mailBody: "success"})
-           
-        }
-
     }
 
    
@@ -74,7 +91,7 @@ class ErrorFormular extends Component{
                 <Form.Label>Describe your problem</Form.Label>
                 <Form.Control as="textarea" placeholder="Your text here..." rows="10" />
             </Form.Group>
-            <Button variant="primary"  onClick={this.printState}>
+            <Button variant="primary"  onClick={this.sendEmail}>
                 Send
             </Button>
         </Form>
