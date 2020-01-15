6df60a7e828d8abd19adb2f57d90857e97e6dcdb
felipeinfantino <felipe.infantino@hotmail.com>
2020-01-15 16:29:28 +0100

    notification to admin done

--- 4FlowDashboards/src/components/dashboard/TodoItem.js
+++ 4FlowDashboards/src/components/dashboard/TodoItem.js
@@ -2,8 +2,49 @@ import React, {Component} from 'react'
 import PropTypes from 'prop-types';
 import {Accordion, Card} from 'react-bootstrap';
 import SubTaskList from './SubTaskList';
+import firebase from "../firebase/Firebase";
+
+var db =  firebase.firestore();
 
 export class TodoItem extends Component {
+
+     deleteTaskAndNotify = async(id) =>{
+        // get the task data
+        const snapshot = await db.collection("PlanerTasks").doc(id).get();
+        const data = snapshot.data();
+        const mailBody = "Your employee needs help : " + JSON.stringify(data);
+        // notify boss
+        try{
+            const response = await fetch('http://localhost:3001/email', {
+                method: 'POST',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    destinations: ["amostuproject@gmail.com"],
+                    subject: "Help needed",
+                    text: mailBody
+                })
+    });
+    console.log(response)
+    // delete task
+    await db.collection("PlanerTasks").doc(id).delete()
+        }catch(e){
+            console.log("error ", e);
+        }
+       
+    }
+
+    getHelp = async (id) =>{
+        try{
+            await this.deleteTaskAndNotify(id)
+            alert("Task assigned to your boss")
+        }catch(e){
+            console.log("Error : ", e);
+            alert("error")
+        }
+    }
 
     render() {
         const {id, title, subTasks} = this.props.todo;
@@ -12,7 +53,12 @@ export class TodoItem extends Component {
     return (
             <Card style={{border: '1px solid black', borderRadius: '5px', margin: '15px', padding: '10px', backgroundColor: '#F8F8F8',}}>
                 <Accordion.Toggle as={Card.Header} eventKey={id}>
-                    {title}
+                    <div>
+                        <p style={{float: "left"}}>
+                        {title} 
+                        </p>
+                        <button className="btn btn-secondary btn-sm" style={{float: "right"}} onClick={() =>this.getHelp(id)} >Help</button>
+                    </div>
                 </Accordion.Toggle>
                 <Accordion.Collapse eventKey={id}>
                 <SubTaskList subTasks={tasks} key={id} todoId={id}
