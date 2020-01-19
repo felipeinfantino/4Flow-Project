c5f0eb50e711c028e7c827655a3d9eac43d15f6b
arehmansaeed62 <abdulrehmansaeed62@yahoo.com>
2020-01-19 23:31:20 +0100

    Fixing styles (in progress)

--- 4FlowDashboards/src/components/planer/RoutingInstr.js
+++ 4FlowDashboards/src/components/planer/RoutingInstr.js
@@ -13,8 +13,8 @@ class RoutingInstr extends React.Component {
                     <a class="excelMac" href="#">Excel Macro Programming file</a>
                 </fieldset>
                 <div class="instructions-buttons">
-                    <button class="btn-lg">Broadcast information to all parties</button><br></br>
-                    <button class="btn-lg">Syncronize dates with parties</button><br></br>
+                    <button>Broadcast information to all parties</button><br></br>
+                    <button>Syncronize dates with parties</button><br></br>
                 </div>
             </div>    
 
