ec276b34d67482d811f07d34e130a57cebd72507
SEBASTIAN BRITO <s.britocorral@gmail.com>
2020-01-19 12:52:56 +0100

    Update TaskFilter use context and update it
    Co-authored-by: Rene <enjilianrene@gmail.com>

--- 4FlowDashboards/src/components/planer/TaskFilter.js
+++ 4FlowDashboards/src/components/planer/TaskFilter.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, {useContext, useState} from 'react';
 import {Popover, ButtonGroup, Button} from 'react-bootstrap'
+import {FilterContext} from "./FilterContext";
 
 
-export default (
-    <Popover id="popover-basic">
-      <Popover.Title as="h3">Filter tasks</Popover.Title>
-      <Popover.Content>
-        <ButtonGroup aria-label="Basic example">
-            <Button variant="outline-secondary">week</Button>
-            <Button variant="outline-secondary">month</Button>
-            <Button variant="outline-secondary">half year</Button>
-            <Button variant="outline-secondary">year</Button>
-        </ButtonGroup>
-      </Popover.Content>
-    </Popover>
-  );
+const TaskFilter = (props) => {
+    const {dispatch} = useContext(FilterContext);
+
+    const handleFilterChange = action => {
+        dispatch({
+            type: action
+        });
+    };
+
+    return (
+        <Popover {...props} id="popover-basic">
+            <Popover.Title as="h3">Filter tasks</Popover.Title>
+            <Popover.Content>
+                <ButtonGroup aria-label="Basic example">
+                    <Button variant="outline-secondary"
+                            onClick={() => handleFilterChange("WEEK")}>week</Button>
+                    <Button variant="outline-secondary"
+                            onClick={() => handleFilterChange("MONTH")}>month</Button>
+                    <Button variant="outline-secondary"
+                            onClick={() => handleFilterChange("HALF_YEAR")}>half year</Button>
+                    <Button variant="outline-secondary"
+                            onClick={() => handleFilterChange("YEAR")}>year</Button>
+                </ButtonGroup>
+            </Popover.Content>
+        </Popover>
+    );
+};
+
+export default TaskFilter
