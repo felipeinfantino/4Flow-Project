08b48e680bdf2bf3d00dc89441e8faa19fb3c9c3
SEBASTIAN BRITO <s.britocorral@gmail.com>
2020-01-22 21:02:31 +0100

    Update FilterContext
    Co-authored-by: Rene <enjilianrene@gmail.com>

--- 4FlowDashboards/src/components/planer/FilterContext.js
+++ 4FlowDashboards/src/components/planer/FilterContext.js
@@ -1,7 +1,8 @@
-import React, {useReducer} from "react";
+import React from "react";
 
 const initialState = {
-    filterValue: "all"
+    filterValue: "all",
+    updateFilterStatePlaner: () => {},
 };
 
 export const FilterContext = React.createContext(initialState);
@@ -12,42 +13,4 @@ export const filterOptions = {
     MONTH: "month",
     HALF_YEAR: "half year",
     YEAR: "year",
-};
-
-const reducer = (state, action) => {
-    switch (action.type) {
-        case "WEEK":
-            return {
-                ...state,
-                filterValue: filterOptions.WEEK
-            };
-        case "MONTH":
-            return {
-                ...state,
-                filterValue: filterOptions.MONTH
-            };
-        case "HALF_YEAR":
-            return {
-                ...state,
-                filterValue: filterOptions.HALF_YEAR
-            };
-        case "YEAR":
-            return {
-                ...state,
-                filterValue: filterOptions.YEAR
-            };
-        default:
-            return state;
-    }
-};
-
-
-export const FilterProvider = ({children}) => {
-    const [state, dispatch] = useReducer(reducer, initialState);
-
-    return (
-        <FilterContext.Provider value={{state, dispatch}}>
-            {children}
-        </FilterContext.Provider>
-    );
 };
