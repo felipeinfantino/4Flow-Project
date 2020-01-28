00438fa42c9cf5b6d489071ac7799041a5061167
samirka9767 <samirehuseyinli@gmail.com>
2020-01-28 20:31:17 +0100

    ui bug fix in 'Done'

--- 4FlowDashboards/src/components/planer/Planer.js
+++ 4FlowDashboards/src/components/planer/Planer.js
@@ -2,15 +2,15 @@ import React from 'react';
 import Todos from '../dashboard/Todos';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AddTodo from '../dashboard/AddTodo';
-import {TaskContext, planerColumnNames} from '../taskManagment/TaskProvider';
-import {uploadTask} from '../taskManagment/TaskUploader';
+import { TaskContext, planerColumnNames } from '../taskManagment/TaskProvider';
+import { uploadTask } from '../taskManagment/TaskUploader';
 import BootstrapSwitchButton from 'bootstrap-switch-button-react';
 import AddToDoPersonal from '../dashboard/AddToDoPersonal';
-import {OverlayTrigger, Button} from 'react-bootstrap';
+import { OverlayTrigger, Button } from 'react-bootstrap';
 import TaskFilter from './TaskFilter';
-import {IoIosOptions} from 'react-icons/io';
-import {IconContext} from "react-icons";
-import {FilterContext, filterOptions} from "./FilterContext";
+import { IoIosOptions } from 'react-icons/io';
+import { IconContext } from "react-icons";
+import { FilterContext, filterOptions } from "./FilterContext";
 import '../../assets/styles/common.css';
 
 
@@ -86,43 +86,44 @@ export class Planer extends React.Component {
                         }}
                     />
                     {this.state.isPrivate ?
-                        <AddToDoPersonal addTodo={this.addTodo}/> :
-                        <AddTodo addTodo={this.addTodo}/>
+                        <AddToDoPersonal addTodo={this.addTodo} /> :
+                        <AddTodo addTodo={this.addTodo} />
                     }
-                    <div style={{display: 'flex'}}>
+                    <div style={{ display: 'flex' }}>
                         {Object.keys(planerColumnNames).map((state) => {
                             return (
-                                <div key={state} style={{width: '25vw'}}>
-                                    <div>
-                                        <h5 style={{
-                                            backgroundColor: '#F8F8F8',
-                                            border: '1px solid black',
-                                            borderRadius: '5px',
-                                            margin: '15px',
-                                            padding: '10px',
-                                            fontStyle: 'bold'
-                                        }}>{planerColumnNames[state]}</h5>
-
+                                <div key={state} style={{ width: '25vw' }}>
+                                    <div style={{
+                                        backgroundColor: '#F8F8F8',
+                                        border: '1px solid black',
+                                        borderRadius: '5px',
+                                        margin: '15px',
+                                        padding: '10px',
+                                    }}>
+                                        <div style={{display:'inline-block'}}>
+                                            <h5>{planerColumnNames[state]}</h5>
+                                        </div>
                                         {planerColumnNames[state] === "Done" &&
-                                        <OverlayTrigger trigger="click" placement="bottom" overlay={TaskFilter}>
-                                            <Button
-                                                style={{
-                                                    float: 'right',
-                                                    position: 'relative',
-                                                    top: '-57px',
-                                                    left: '-35px'
-                                                }}
-                                                variant="secondary">
+                                            <div style={{display:'inline-block', float: 'right'}}>
+                                                <OverlayTrigger trigger="click" placement="bottom" overlay={TaskFilter}>
+                                                    <Button
+                                                        style={{
+                                                            float: 'right'
+                                                        }}
+                                                        variant="secondary">
 
-                                                <IconContext.Provider
-                                                    value={{size: "1.2em", className: "global-class-name"}}>
-                                                    <div>
-                                                        <IoIosOptions/>
-                                                    </div>
-                                                </IconContext.Provider>
-                                            </Button>
-                                        </OverlayTrigger>}
+                                                        <IconContext.Provider
+                                                            value={{ size: "1.2em", className: "global-class-name" }}>
+                                                            <div>
+                                                                <IoIosOptions />
+                                                            </div>
+                                                        </IconContext.Provider>
+                                                    </Button>
+                                                </OverlayTrigger>
+                                            </div>}
                                     </div>
+
+
 
                                     <TaskContext.Consumer>
                                         {(context) => (
@@ -132,8 +133,8 @@ export class Planer extends React.Component {
                                                     todo.type === (this.state.isPrivate ? 'personal' : 'pipeline') &&
                                                     (planerColumnNames[state] === "Done" ? this.checkFilter(todo.deadline) : true)
                                                 )}
-                                                   toggleSubTask={context.togglePlanerSubtask}
-                                                   history={this.props.history}
+                                                toggleSubTask={context.togglePlanerSubtask}
+                                                history={this.props.history}
                                             />
                                         )}
                                     </TaskContext.Consumer>
