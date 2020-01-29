b8edbed2d9abb48144bbf4eb64eadd6d271a8cad
felipeinfantino <felipe.infantino@hotmail.com>
2020-01-29 16:36:43 +0100

    reminder fixed

--- 4FlowDashboards/src/components/email/deadlineReminder.js
+++ 4FlowDashboards/src/components/email/deadlineReminder.js
@@ -1,74 +1,83 @@
-import React from 'react';
+import React, { Component } from "react";
 import firebase from "../firebase/Firebase";
-import {Redirect} from "react-router";
-var db =  firebase.firestore();
+var db = firebase.firestore();
 
-var list = []
-
-function getData(){
-    var currentDateInHours = Date.now()/(1000*60*60);
-        var oneDay = 24;
-        db.collection("PlanerTasks").get().then(function(querySnapshot) {
-            querySnapshot.forEach(function(doc) {
-            if (typeof(doc.get('deadline')) !== 'undefined'){
-                var hours = Date.parse(doc.get('deadline').toDate())/(1000*60*60);
-                if(hours-currentDateInHours <= oneDay && hours-currentDateInHours > 0){
-                    list.push(doc.id);
-                }
-                
-            }
-        });
-    });   
-
+function splitDate(date) {
+  return {
+    day: date.getDay(),
+    month: date.getMonth(),
+    year: date.getFullYear()
+  };
 }
-   async function sendEmail (){
 
-         /*return fetch('http://localhost:3001/email', {
-            method: 'POST',
-            headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                destinations: ["amostuproject@gmail.com"],
-                subject: "Deadline-Reminder",
-                text: list.join()
-            })
-        })*/
-        try{
-           const response = await fetch('http://localhost:3001/email', {
-                method: 'POST',
-                headers: {
-                    Accept: 'application/json',
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    destinations: ["amostuproject@gmail.com"],
-                    subject: "MY TEST",
-                    text: list.join()
-                })
-    });
-    console.log(response)
-    alert("Email successfully sent");
-        }catch(e){
-            console.log("error ", e);
+export default class DeadlineRem extends Component {
+  async getData() {
+    const currentDate = new Date();
+    const splittedDate = splitDate(currentDate);
+    const currentDay = splittedDate.day;
+    const currentMonth = splittedDate.month;
+    const currentYear = splittedDate.year;
+    const data = [];
+    try {
+      const snapshot = await db.collection("PlanerTasks").get();
+      snapshot.forEach(doc => {
+        const deadline = doc.get("deadline");
+        const status = doc.get("status");
+        if (deadline && status !== "Done") {
+          const { day, month, year } = splitDate(deadline.toDate());
+          if (
+            currentDay === day &&
+            currentMonth === month &&
+            currentYear === year
+          ) {
+            console.log("pushing")
+            data.push(doc.get("title"));
+          }
         }
+      });
+    } catch (e) {}
+    return data;
+  }
+
+  async sendEmail() {
+    const data = await this.getData();
+    if(data.length === 0){
+        alert("no tasks to remind");
+        return;
     }
+    const message = "Reminder : Following tasks are due today : \n" + data.join(" ");
+    try {
+      const response = await fetch("http://localhost:3001/email", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          destinations: ["amostuproject@gmail.com"],
+          subject: "REMINDER",
+          text: message
+        })
+      });
+      alert("Email successfully sent");
+    } catch (e) {
+      console.log("error ", e);
+    }
+  }
 
-function deadlineRem(){
-        getData()
-        return (
-            <div>
-                <form id="emailForm">
-                    <div>
-                        <button type="submit" className="btn btn-primary submit-button" onClick={sendEmail}>Send</button>
-                    </div>
-                </form>
-            </div>
-        )
-    
-    
-    
+  render() {
+    return (
+      <div>
+        <div>
+          <button
+            type="btn"
+            className="btn btn-primary submit-button"
+            onClick={() => this.sendEmail()}
+          >
+            Send
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default deadlineRem;
